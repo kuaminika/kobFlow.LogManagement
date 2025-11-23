9bbaa4b795c81498074288d3b41b984718482875
Kuaminika <kuaminika@gmail.com>
2025-11-23 05:14:20 -0500

    updated .env

--- kobflow.LogManager/vite.config.js
+++ kobflow.LogManager/vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+const BASE_URL = process.env.BASE_URL || '';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base:BASE_URL,
   plugins: [
     react({
       babel: {
